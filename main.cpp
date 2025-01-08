20157c79168006fc61c6da9f11a1ab4a8b0e7811
Michael Dillender <dillemic000@gmail.com>
2025-01-08 13:41:43 -0500

    should work?

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<fstream>
+#include <sstream>
 #include<numeric>
 #include <random>
 #include <vector>
@@ -32,77 +33,6 @@ double FRET_denom= 4*(.023*.023);// = 4 sigma^2
 double delta_ss=.038;
 double FRET_scaling = 23; // (C/dij^6)
 double top_density=1;
-                          //0 1  2  3 4 5 6 7 8 9 10 11
-array<uint16_t,12> rev_dir={9,10,11,8,7,6,5,4,3,0,1, 2};
-
-struct pos{
-    pos(uint16_t x,uint16_t y, uint16_t z):x{x},y{y},z{z}{}
-    pos(pos &p0):x{p0.x},y{p0.y},z{p0.z}{}
-    pos(pos &p0,uint16_t d):x{p0.x},y{p0.y},z{p0.z}{
-        go_in_direction(d);
-    }
-    void go_in_direction(uint16_t d){
-        if(d<3){
-            if(d==1){ //only change y
-                if(z % 2 ==0){ x--;
-                }else{ x++; }
-            } else if(d==2){ // d==2, change x and y
-                if(z % 2 == 0){ // decrease y
-                    if (y % 2 == 0){ x--; }
-                    y--;
-                } else{ // increase y
-                    if (y % 2 == 1){ x++; }
-                    y++;
-                }
-            }
-            z++;
-        }else if(d<9){
-            if(d<5){ // d = 3 or 4, up
-                if(y%2==0){ x--; }
-                if(d==4){ x++; }
-                y++;
-            }else if(d<7){ // d = 5 or 6, just along x
-                if(d==5){ x--;
-                }else{ x++; } //if d=6
-            }else{ // d = 7 or 8
-                if(y%2==0){ x--; }
-                if(d==8){ x++; }
-                y--;
-            }
-        }else if(d<12){
-            if(d==10){ // only change x
-                if(z % 2 ==0){ x--;
-                }else{ x++; }
-            } else if(d==11){ // d==2, change x and y
-                if(z % 2 == 0){ // decrease y
-                    if (y % 2 == 0){ x--; }
-                    y--;
-                } else{ // increase y
-                    if (y % 2 == 1){ x++; }
-                    y++;
-                }
-            }
-            z--;
-        }
-    }
-    void get_real_pos(double vec[]){
-        vec[0]=x;
-        vec[1]=y*.86602540378;
-        vec[2]=z*0.81649658092;
-
-        if(y%2==1){
-            vec[0]+=0.5;
-        }
-        if(z%2==1){
-            vec[0]+=0.5;
-            vec[1]-=0.25;
-        }
-    }
-    void print(){cout<<"("<<x<<","<<y<<","<<z<<") ";}
-    void print_sub(pos &p1){cout<<"("<<x-p1.x<<","<<y-p1.y<<","<<z-p1.z<<") ";}
-    void operator=(const pos& p1){x=p1.x; y=p1.y; z=p1.z;};
-    uint16_t x; uint16_t y; uint16_t z;
-};
 
 
 void save_as_csv(const std::vector<std::vector<uint32_t>>& data, const std::string& filename) {
@@ -122,10 +52,13 @@ void save_as_csv(const std::vector<std::vector<uint32_t>>& data, const std::stri
     std::cout << "CSV saved to " << filename << std::endl;
 }
 
-double rates[width][width][layers][13];
-bool absent[width][width][layers];
-vector<vector<vector<double>>> energies(width,vector<vector<double>>(width,vector<double>(layers,0)));
+vector<double> energies;
+vector<vector<double>> rates;
 
+vector<vector<uint32_t>> nns;
+uint32_t Np=0;
+uint32_t Lbox=0;
+std::vector<double> qdx, qdy, qdz;
 
 double dprod(double v1[], double v2[]){
     return v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2];
@@ -144,29 +77,23 @@ void setQDs(){
     double energy_sigma=.030;
     double theta=0;
     double phi=0;
-    signed char dipoles[width][width][layers][3]={}; // unit vector of the dipole * 100
+
+    vector<double> dix; // unit vector of the dipole * 100
+    vector<double> diy; // unit vector of the dipole * 100
+    vector<double> diz; // unit vector of the dipole * 100
 
     random_device rd_e;  // Seed
     mt19937 generator_e(rd_e());  // Random number generator
     normal_distribution<double> e_dist(energy_mu, energy_sigma);
 
     // Fill the 3D array with random values from the normal distribution
-    for (int i = 0; i < width; ++i) {
-        for (int j = 0; j < width; ++j) {
-            for (int k = 0; k < layers; ++k) {
-                energies[i][j][k] = e_dist(generator_e);
-                theta = 3.14159*(rand() % 360)/180;
-                phi = 3.14159*(rand() % 360)/180;
-                dipoles[i][j][k][0]=(signed char)(100*cos(theta)*sin(phi));
-                dipoles[i][j][k][1]=(signed char)(100*sin(theta)*sin(phi));
-                dipoles[i][j][k][2]=(signed char)(100*cos(phi));
-                if(k==layers-1 && top_density<1){
-                    absent[i][j][k] = 1000*top_density<(rand()%1000); //absent if rand < % absent
-                } else{
-                    absent[i][j][k] = false;
-                }
-            }
-        }
+    for (uint32_t i = 0; i < Np; ++i) {
+        energies.push_back(e_dist(generator_e));
+        theta = 3.14159*(rand() % 360)/180;
+        phi = 3.14159*(rand() % 360)/180;
+        dix.push_back(cos(theta)*sin(phi));
+        diy.push_back(sin(theta)*sin(phi));
+        diz.push_back(cos(phi));
     }
 
     double kappa;
@@ -174,9 +101,38 @@ void setQDs(){
     double E_d;
     double r_d[3]={};
     double r_a[3]={};
+    double mu_d[3]={};
+    double mu_a[3]={};
     double d_da[3]={};
     double d_da_mag;
-    for (uint16_t z=0; z<layers; z++) {
+    for(uint32_t id=0; id<Np; id++){
+        vector<double> rates_i=vector<double>(13,0);
+        rates_i[12]=1;
+        E_d=energies[id];
+        r_d[0]=qdx[id]; r_d[1]=qdy[id]; r_d[2]=qdz[id];
+        mu_d[0]=dix[id]; mu_d[1]=dix[id]; mu_d[2]=dix[id];
+        for(uint32_t j=0;j<12;j++){
+            uint32_t ia=nns[id][j];
+            E_a=energies[ia];
+            r_a[0]=qdx[ia]; r_a[1]=qdy[ia]; r_a[2]=qdz[ia];
+            mu_a[0]=dix[ia]; mu_a[1]=dix[ia]; mu_a[2]=dix[ia];
+
+            d_da[0]=r_a[0]-r_d[0]; d_da[1]=r_a[1]-r_d[1]; d_da[2]=r_a[2]-r_d[2];
+            d_da_mag=d_da[0]*d_da[0]+d_da[1]*d_da[1]+d_da[2]*d_da[2];
+            //cout<<"d"<<d_da_mag<<"\n";
+            //cout<<"distance";
+            kappa= dprod(mu_d,mu_a)-3* dprod(mu_d,d_da)*dprod(mu_a,d_da)/d_da_mag;
+            rates_i[j]=FRET_scaling*kappa*kappa*exp(-pow(E_d-E_a-delta_ss,2)/FRET_denom)/pow(.5*(E_d+E_a-delta_ss),4);
+            rates_i[j]=rates_i[j]*pow(4/d_da_mag,3);
+            rates_i[12]+=rates_i[j];
+            cout<<id<<" ("<<E_d<<") -> "<<ia<<" ("<<E_a<<") | rate = "<<rates_i[j]<<", kappa = "<<kappa<<", d = "<<d_da_mag<<"\n";
+            //cout<<" | d_da = ("<<d_da[0]<<","<<d_da[1]<<","<<d_da[2]<<") ";
+        }
+        rates.push_back(rates_i);
+        //cout<<"("<<x<<","<<y<<","<<z<<") E = "<<E_d<<", total rate = "<<rates[x][y][z][12]<<"\n";
+    }
+
+    /*for (uint16_t z=0; z<layers; z++) {
         uint16_t dmin = (z<layers-1)?0:3;
         uint16_t dmax = (z>0)?12:9;
         for (uint16_t x=0; x<width; x++){
@@ -198,14 +154,12 @@ void setQDs(){
                         kappa= dprod(dipoles[x][y][z],dipoles[p1.x][p1.y][p1.z])-3* dprod(dipoles[x][y][z],d_da)*dprod(dipoles[p1.x][p1.y][p1.z],d_da)/d_da_mag;
                         rates[x][y][z][d]=FRET_scaling*kappa*kappa*exp(-pow(E_d-E_a-delta_ss,2)/FRET_denom)/pow(.5*(E_d+E_a-delta_ss),4);
                         rates[x][y][z][12]+=rates[x][y][z][d];
-                        //cout<<"\n"; p0.print(); cout<<", "<<E_d<<" -> "; p1.print(); cout<<", "<<E_a<<" | rate = "<<rates[x][y][z][d]<<", kappa = "<<kappa;
-                        //cout<<" | d_da = ("<<d_da[0]<<","<<d_da[1]<<","<<d_da[2]<<") ";
+
                     }
                 }
-                //cout<<"("<<x<<","<<y<<","<<z<<") E = "<<E_d<<", total rate = "<<rates[x][y][z][12]<<"\n";
             }
         }
-    }
+    }*/
 }
 
 vector<vector<uint32_t>> apd;
@@ -224,12 +178,12 @@ random_device rd;
 mt19937 rand_gen (rd ());
 exponential_distribution<> exp_dist(base_rate);
 
-void sim_particle(pos p, double t){
-    double transfer_time=exp_dist(rd)/rates[p.x][p.y][p.z][12]; //should be base_rate/rates[x][y][z], but rates[][][] is already normalized by base rate
-    double trans_rand=((double)rand()/(double)RAND_MAX)*rates[p.x][p.y][p.z][12];// decides how it decays, either FRET or radiative
+void sim_particle(uint32_t i, double t){
+    double transfer_time=exp_dist(rd)/rates[i][12]; //should be base_rate/rates[x][y][z], but rates[][][] is already normalized by base rate
+    double trans_rand=((double)rand()/(double)RAND_MAX)*rates[i][12];// decides how it decays, either FRET or radiative
     //cout<<trans_rand<<"\n";
     if(trans_rand<1){
-        uint16_t e_bin=(uint16_t)((energies[p.x][p.y][p.z]-energy_min)/energy_step);
+        uint16_t e_bin=(uint16_t)((energies[i]-energy_min)/energy_step);
         uint16_t t_bin=(uint16_t)((t+transfer_time)/time_step);
         if(e_bin<energy_resolution && t_bin<time_resolution){
             //cout<<"emission at "<<", "<<t_bin<<"\n";
@@ -244,7 +198,7 @@ void sim_particle(pos p, double t){
         double cumul_rate=1;
         uint16_t d=0;
         for (; d<12; d++){
-            cumul_rate += rates[p.x][p.y][p.z][d];
+            cumul_rate += rates[i][d];
             if(trans_rand<cumul_rate){
                 break;
             }
@@ -255,28 +209,97 @@ void sim_particle(pos p, double t){
         }
         cout<<"\n";*/
 
-        p.go_in_direction(d);
-        sim_particle(p,t+transfer_time);
+
+        sim_particle(nns[i][d],t+transfer_time);
     }
 }
 
 
-int main(/*int argc=0, char** argv=nullptr*/){
-    setQDs();
-    srand((unsigned) time(NULL));
+void readCSV(const string& filename, vector<double>& x, vector<double>& y, vector<double>& z) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file " << filename << std::endl;
+        exit(1);
+        return;
+    }
 
+    std::string line;
+    double maxx=0;
+    while (std::getline(file, line)) {
+        std::istringstream ss(line);
+        std::string token;
+        vector<uint32_t> nearest;
+        double x_val, y_val, z_val;
+
+        // Parse x
+        if (std::getline(ss, token, ',')) {
+            x_val = std::stod(token);
+        } else {
+            continue; // Skip if incomplete line
+        }
+
+        if(x_val>maxx){
+            maxx=x_val;
+        }
+        // Parse y
+        if (std::getline(ss, token, ',')) {
+            y_val = std::stod(token);
+        } else {
+            continue; // Skip if incomplete line
+        }
+
+        // Parse z
+        if (std::getline(ss, token, ',')) {
+            z_val = std::stod(token);
+        } else {
+            continue; // Skip if incomplete line
+        }
+        while(std::getline(ss,token,',')){
+            nearest.push_back(static_cast<uint32_t>(std::stoul(token)));
+        }
+
+        // Add to vectors
+        x.push_back(x_val);
+        y.push_back(y_val);
+        z.push_back(z_val);
+        nns.push_back(nearest);
+        Np++;
+    }
+    Lbox=(int32_t)maxx+1;
+
+    file.close();
+}
+
+int main(/*int argc=0, char** argv=nullptr*/){
+    srand((unsigned) time(NULL));
+    std::string filename = "dots_N2880_w30.csv"; // Replace with your CSV file path
+    readCSV(filename, qdx, qdy, qdz);
+    cout<<" box size is "<<Lbox<<" \n";
+    setQDs();
+
+    // Print the vectors for verification
+    //std::cout << std::endl;
+    /*for (uint32_t i=0;i<Np;i++){
+        cout<<i<<" ("<<qdx[i]<<","<<qdy[i]<<","<<qdz[i]<<") : ";
+        for(uint32_t j:nns[i]){
+            cout<<j<<", ";
+        }
+        cout<<"\n";
+    }*/
 
     apd=vector<vector<uint32_t>>(time_resolution,vector<uint32_t>(energy_resolution));
-    pos p(0,0,0);
-    int w_edge=15;
+    //pos p(0,0,0);
+    int w_edge=5;
     if(width<w_edge*2+2){cout<<"increase width\n"; return 0;}
-    int w_mid=width-2*w_edge;
-    for(uint32_t i=0; i<3000000; i++){
-        p=pos((uint16_t)(w_edge+rand()%w_mid),(uint16_t)(w_edge+rand()%w_mid),(uint16_t)(rand()%layers));
-        if(absent[p.x][p.y][p.z]){i--;continue;}
-        sim_particle(p,0);
+    for(uint32_t iter=0; iter<1000; iter++){
+        uint32_t i = (uint32_t)(rand()%Np);
+        if(abs(qdx[i])+w_edge>Lbox){iter--; continue;}
+        if(abs(qdy[i])+w_edge>Lbox){iter--; continue;}
+        //p=pos((uint16_t)(w_edge+rand()%w_mid),(uint16_t)(w_edge+rand()%w_mid),(uint16_t)(rand()%layers));
+        //if(absent[p.x][p.y][p.z]){i--;continue;}
+        sim_particle(i,0);
     }
-    string name="output2_"+to_string(layers)+"L_d"+to_string(int(round(top_density*100)))+"_C"+to_string(int(FRET_scaling))+".csv";
+    string name="APD_"+to_string(layers)+"L_d"+to_string(int(round(top_density*100)))+"_C"+to_string(int(FRET_scaling))+".csv";
     save_as_csv(apd, name);
     return 0;
 }
